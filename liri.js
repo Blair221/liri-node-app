277b0984e3f339adcae13806cc69b3c4c66a33b8
Blair Brownlie <bbrownli@asu.edu>
2019-04-23 00:21:05 -0700

    piecing together the different inputs from the command line

--- liri.js
+++ liri.js
@@ -11,8 +11,11 @@ var spotify = new Spotify(keys.spotify);
 // console.log(spotify);
 
 var liriCommand = process.argv[2];
+var userInput = process.argv.slice(3).join(' ');
 
 console.log(liriCommand);
+console.log(userInput);
+
 
 
 
